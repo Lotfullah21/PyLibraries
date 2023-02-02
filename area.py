e1796dece36512180334f99e516a3963cfc74791
Lotfullah <85621380+Lotfullah21@users.noreply.github.com>
2023-02-03 01:52:55 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,22 @@
+
+import math
+
+inputOk = False
+
+while not inputOk:
+    base = 10
+    print("Okay")
+    inputOk = True
+
+inputOk = False
+while not inputOk:
+    
+    height = 2
+    inputOk = True
+
+hyp = math.sqrt(height**2 + base**2)
+
+print("square with the heght of ", height, "base of", base, "is", hyp)
+
+
+
